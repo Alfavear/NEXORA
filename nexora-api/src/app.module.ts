b6ae5ec630c79b1c98c3fc88149ac3a2b6b440ad
Alfavear <138264363+Alfavear@users.noreply.github.com>
2026-03-30 12:55:40 -0500

    feat: full schema + sales + returns base

--- nexora-api/src/app.module.ts
+++ nexora-api/src/app.module.ts
@@ -4,20 +4,22 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ItemsModule } from './items/items.module';
-import { CategoriesModule } from './categories/categories.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { CustomersModule } from './customers/customers.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ItemsModule } from './items/items.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     UsersModule,
-    CategoriesModule,
-    ItemsModule,
     SuppliersModule,
     CustomersModule,
+    CategoriesModule,
+    ItemsModule,
+    SalesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
